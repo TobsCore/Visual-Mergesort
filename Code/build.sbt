2bd3b5cbd76cecf920a49b890cbed421594e1896
Tobias Kerst <tobias.kerst@tobscore.com>
2016-08-19 12:05:16 +0200

    Changes example to use FXML file

--- Code/build.sbt
+++ Code/build.sbt
@@ -5,3 +5,7 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.scalafx" %% "scalafx" % "8.0.92-R10"
+
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
+
+libraryDependencies += "org.scalafx" %% "scalafxml-core-sfx8" % "0.2.2"
--- Code/src/main/scala/example/ExampleApplication.scala
+++ Code/src/main/scala/example/ExampleApplication.scala
@@ -1,46 +1,24 @@
 package example
 
+import java.io.IOException
+
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
-import scalafx.scene.paint.Color.{PaleGreen, SeaGreen, Cyan, DodgerBlue, Black}
-import scalafx.scene.paint.{LinearGradient, Stops}
-import scalafx.scene.text.Text
+import scalafx.Includes._
+import scalafxml.core.{FXMLView, NoDependencyResolver}
 
 object ExampleApplication extends JFXApp {
-
-  stage = new PrimaryStage {
-    title = "Beispiel Anwendung für Patrick"
-    scene = new Scene {
-      fill = Black
-      content = new HBox {
-        padding = Insets(20)
-        children = Seq(
-          new Text {
-            text = "#Yolo "
-            style = "-fx-font-size: 48pt"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(PaleGreen, SeaGreen))
-          },
-          new Text {
-            text = "#Swag"
-            style = "-fx-font-size: 48pt"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Cyan, DodgerBlue)
-            )
-            effect = new DropShadow {
-              color = DodgerBlue
-              radius = 25
-              spread = 0.25
-            }
-          }
-        )
-      }
-    }
+  val resource = getClass.getResource("BasicApplication_css/BasicApplication_css.fxml")
+  if (resource == null) {
+    throw new IOException("Cannot load resource: AdoptionForm.fxml")
   }
+
+  val root = FXMLView(resource, NoDependencyResolver)
+
+  stage = new PrimaryStage() {
+    title = "FXML GridPane Demo"
+    scene = new Scene(root)
+  }
+
 }
